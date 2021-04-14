40ba81b0e5b6f87507547b297d9ef779dbb26620
Nusrat Jahan Mim <77601025+Nnusratjahanmim@users.noreply.github.com>
2021-04-14 15:01:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+
+$(function(){
+        $('.tab_container').accordion({
+          clickItem : $('.same_open'),
+          contentItem : $('.same_open_content'),
+          contentWidth : '340px',
+          closeTime : 500,
+          openTime : 500,
+          tmpInput : $('#tmp'),
+          clickItemBgColor : '#9d8757',
+          contentItemBgColor : '#e2dbcc'
+        });
+      });
+
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+$('#myClouds').Klouds({
+    layerCount: 5,
+    speed: 1,
+    cloudColor1: '#19b2cc',
+    cloudColor2: '#fafafa',
+})
+
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+});
